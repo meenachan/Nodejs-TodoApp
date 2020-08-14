b04cb6779afa7c1ec88c1379ff8806489a538bfd
Meenakshi Chauhan <mcquitypie3@gmail.com>
2020-08-14 17:54:12 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://meena:123@cluster0.bicz7.mongodb.net/todo-dev?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://meena:123@cluster0.bicz7.mongodb.net/tododb?retryWrites=true&w=majority'
 }
